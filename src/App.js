2e5c1dbca684579b7f90a4db8c026a0ea1e4ad8e
Thushan Dodanwela <dodanwelathushan@gmail.com>
2022-10-02 17:17:01 +0530

    Allocation View

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import { Col, Row } from "react-bootstrap";
 import Lecturers from "./pages/Lecturers/Lecturers";
 import { useState } from "react";
 import Allocations from "./pages/Allocations/Allocations";
+import AllocationsView from "./pages/AllocationsView/AllocationsView";
 
 function App() {
-
-  const [navbar,setNavbar] = useState('Dashboard')
+  const [navbar, setNavbar] = useState("Dashboard");
 
   return (
     <div className="App d-flex">
@@ -22,10 +22,19 @@ function App() {
           <Navbar section={navbar} />
           <Row className="m-0">
             <Routes>
-              <Route path="/" element={<Home setNavbar={setNavbar}/>} />
-              <Route path="/modules" element={<Modules setNavbar={setNavbar}/>} />
-              <Route path="/lecturers" element={<Lecturers setNavbar={setNavbar}/>} />
-              <Route path="/allocations" element={<Allocations setNavbar={setNavbar}/>} />
+              <Route path="/" element={<Home setNavbar={setNavbar} />} />
+              <Route
+                path="/modules"
+                element={<Modules setNavbar={setNavbar} />}
+              />
+              <Route
+                path="/lecturers"
+                element={<Lecturers setNavbar={setNavbar} />}
+              />
+              <Route
+                path="/allocations"
+                element={<AllocationsView setNavbar={setNavbar} />}
+              />
             </Routes>
           </Row>
         </Col>
